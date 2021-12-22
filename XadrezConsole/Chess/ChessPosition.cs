ea58959b08132938f30d0fde23e9e9c7851b46a4
João Victor de Oliveira <victorcar043@gmail.com>
2021-12-22 15:50:37 -0300

    Criate class Chess Position

---
+++ XadrezConsole/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using XadrezConsole.Board;
+
+namespace XadrezConsole.Chess
+{
+    internal class ChessPosition
+    {
+        public char Colunm { get; set; }
+        public int Row { get; set; }
+
+        public ChessPosition(char colunm, int row)
+        {
+            Colunm = colunm;
+            Row = row;
+        }
+
+        public override string ToString()
+        {
+            return $"{Colunm}{Row}";
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Colunm - 'a');
+        }
+    }
+}
